95ebf63c5a7a59f9111bc20331890136031cdd6d
HelenR462 <69416811+HelenR462@users.noreply.github.com>
2022-07-18 22:43:30 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,30 +5,12 @@ const addTaskButton = document
   .querySelector("#submit")
   .addEventListener("click", (e) => addTask(e));
 
-// if (erand.value.length > 0) return (submit.disabled = false);
-//  (submit.disabled = true);
-
-// erand.addEventListener("click", (e) => {
-//   if (erand.value.length > 0) {
-//     addTaskButton.disabled = false;
-//   } else {
-//     addTaskButton.disabled = true;
-//   }
-// });
-
 function addTask(e) {
   e.preventDefault();
   if (erand.value === "") return;
-  // const listDiv = document.createElement("div");
-  // listDiv.classList.add("list");
-
-  // const newUl = document.createElement("ul");
-  // newUl.setAttribute("id", "output");
-  // listDiv.appendChild(newUl);
 
   const newLi = document.createElement("li");
   newLi.classList.add("task");
-  // newUl.appendChild(newLi);
 
   // CHECKBOX
   const doneCheckbox = document.createElement("input");
@@ -56,7 +38,7 @@ function addTask(e) {
   trashButton.classList.add("btn");
 
   trashButton.onclick = (e) => {
-    console.log(e.target.parentElement.parentElement.parentNode.remove(newLi));
+    e.target.parentElement.parentElement.remove(newLi);
   };
 
   newLi.appendChild(trashButton);
@@ -70,5 +52,7 @@ const todoDiv = document.getElementsByClassName("todo");
 const resetButton = document.getElementById("reset");
 
 resetButton.onclick = (e) => {
-  console.log(e.target.parentElement.parentElement.removeChild(todo));
+  e.target.parentElement.parentElement.removeChild(todo);
+
+  location.reload();
 };
